7bdc434a80b39ef826c7d843ada89186c0637c89
Myles Scolnick <myles@marimo.io>
2025-04-29 15:09:41 -0400

    widgets: add itables and pygwalker

--- gallery/app/utils/notebook.ts
+++ gallery/app/utils/notebook.ts
@@ -1,4 +1,5 @@
 import type { BuiltWithAnyWidget } from "~/model/types";
+import { dedent } from "ts-dedent";
 
 export function resolveNotebookURL(widget: BuiltWithAnyWidget) {
 	const queryParams = new URLSearchParams();
@@ -6,7 +7,10 @@ export function resolveNotebookURL(widget: BuiltWithAnyWidget) {
 	if (widget.notebookUrl) {
 		queryParams.set("src", widget.notebookUrl);
 	} else if (widget.notebookCode) {
-		queryParams.set("code", encodeURIComponent(widget.notebookCode));
+		queryParams.set(
+			"code",
+			encodeURIComponent(createNotebookFromCode(widget.notebookCode)),
+		);
 	} else if (widget.githubRepo) {
 		let guessPackageName =
 			widget.packageName || widget.githubRepo.split("/").pop();
@@ -43,4 +47,29 @@ async def _():
 		notebookUrl: `https://marimo.app?${queryParams.toString()}`,
 		embedUrl: `https://marimo.app?${embedQueryParams.toString()}`,
 	};
+}
+
+function createNotebookFromCode(code: string) {
+	if (code.includes("app = marimo.App()")) {
+		return dedent(code);
+	}
+
+	const dedentedCode = dedent(code);
+	return `
+import marimo
+
+app = marimo.App()
+
+@app.cell
+async def _():
+${indentCode(dedentedCode, "    ")}
+    return
+`;
+}
+
+function indentCode(code: string, indent: string) {
+	return code
+		.split("\n")
+		.map((line) => `${indent}${line}`)
+		.join("\n");
 }
